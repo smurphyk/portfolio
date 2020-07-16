e22fd12f3672c1dc93532369646ca76f0f9324ca
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-16 15:32:08 -0500

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sean Murphy`,
+    description: `This is a portfolio that showcases the work of your favorite developer, Sean Murphy`,
     author: `@gatsbyjs`,
   },
   plugins: [
