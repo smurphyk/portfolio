39dbcd2f69794f8d0c2277f87f698c5adc99dfd3
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-17 11:28:57 -0500

    Began refining file structure and overall look of site.

--- src/components/about.js
+++ src/components/about.js
@@ -2,3 +2,11 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
+
+import Header from "../components/header"
+
+const About = () => {
+  return <Header />
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
+    <Header />
     <SEO title="Home" />
-    <h1>Welcome to my Portfolio!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>SEAN MURPHY</h1>
+    <p>Introducing Development with a Sense of Humor</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
