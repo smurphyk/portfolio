a2803d1c179d09ac9d19445c958e84fa91c2c9a1
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-17 11:40:01 -0500

    Began refining file structure and overall look of site.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// Styling Imports
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "../styles/styles.scss"
+
+// Component Imports
 import Header from "../components/header"
 
 const IndexPage = () => (
@@ -13,7 +17,6 @@ const IndexPage = () => (
     <h1>SEAN MURPHY</h1>
     <p>Introducing Development with a Sense of Humor</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
 )
 
